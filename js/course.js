0460c9b8f2a07409a5cebe2d71dd2f5ccbac3dfd
Venilton FalvoJr <falvojr@gmail.com>
2021-11-05 14:36:34 -0300

    Copiar CSV Base de Treinamentos

--- js/course.js
+++ js/course.js
@@ -1,11 +1,30 @@
 (function() {
     'use strict';
 
-    window.EducationCustomizations.createAction('Copiar Link na Plataforma', async () => {
+    function getCourseDioMeUrl() {
         const slug = document.getElementById('id_slug').value;
         const uuid = document.querySelector('.field-uuid > div > div').innerText;
-        const urlCurso = `https://web.dio.me/course/${slug}/learning/${uuid}`;
-        await navigator.clipboard.writeText(urlCurso);
+        return `https://web.dio.me/course/${slug}/learning/${uuid}`;
+    }
+
+    window.EducationCustomizations.createAction('Copiar Link DIO.ME', async () => {
+        const courseDioMeUrl = getCourseDioMeUrl();
+        await navigator.clipboard.writeText(courseDioMeUrl);
+    });
+
+    window.EducationCustomizations.createAction('Copiar CSV Base de Treinamentos', async () => {
+        const id = window.location.pathname.split('/')[3];
+        const editorial = '';
+        const name = document.querySelector('#id_name').value;
+        const tecnology = document.querySelector('#id_coursehasskills_set-0-skill > option:selected').innerText;
+        const duration = document.querySelector('#id_workload').value;
+        const author = document.querySelector('#id_author_on_deck > div > span.tag').innerText;
+        const gender = '';
+        const stack = '';
+        const linkDioMe = getCourseDioMeUrl();
+        const LinkAdmin = window.location.href;
+        const courseCsv = `${id},${editorial},${name},${tecnology},${duration},${author},${gender},${stack},${linkDioMe},${LinkAdmin}`;
+        await navigator.clipboard.writeText(courseCsv);
     });
 
 })();
