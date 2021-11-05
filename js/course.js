1efb18bda110c29576cfaddb32cabc3e0b8ed589
Venilton FalvoJr <falvojr@gmail.com>
2021-11-05 15:18:36 -0300

    Aumento da complexidade de código para evitar demissão

--- js/course.js
+++ js/course.js
@@ -2,8 +2,8 @@
     'use strict';
 
     function getCourseDioMeUrl() {
-        const slug = document.getElementById('id_slug').value;
-        const uuid = document.querySelector('.field-uuid > div > div').innerText;
+        const slug = document.getElementById('id_slug')?.value || '';
+        const uuid = document.querySelector('.field-uuid > div > div')?.innerText || '';
         return `https://web.dio.me/course/${slug}/learning/${uuid}`;
     }
 
@@ -13,12 +13,12 @@
     });
 
     window.EducationCustomizations.createAction('Copiar CSV Base de Treinamentos', async () => {
-        const id = window.location.pathname.split('/')[3];
+        const [,,,,id] = window.location.pathname.split('/');
         const editorial = '';
-        const name = document.querySelector('#id_name').value;
-        const tecnology = document.querySelector('#id_coursehasskills_set-0-skill > option[selected]').innerText;
-        const duration = document.querySelector('#id_workload').value;
-        const author = document.querySelector('#id_author_on_deck > div > span.tag').innerText;
+        const name = document.querySelector('#id_name')?.value || '';
+        const tecnology = document.querySelector('#id_coursehasskills_set-0-skill > option[selected]')?.innerText || '';
+        const duration = document.querySelector('#id_workload')?.value || '';
+        const author = document.querySelector('#id_author_on_deck > div > span.tag')?.innerText || '';
         const gender = '';
         const stack = '';
         const linkDioMe = getCourseDioMeUrl();
