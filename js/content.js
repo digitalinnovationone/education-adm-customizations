7582d3598aded49fcce1419383999f4c7608c89d
Venilton FalvoJr <falvojr@gmail.com>
2022-08-10 16:18:50 -0300

    Primeira versão para testes de normalização de links.

--- js/content.js
+++ js/content.js
@@ -6,6 +6,19 @@
 
     const contentType = document.querySelector('.field-type > div > div')?.innerText || '';
     if (contentType == 'Texto') {
-        console.log('É texto!');
+        document.querySelectorAll('[type="submit"][value^="Salvar"').forEach(saveInput => {
+            saveInput.addEventListener('mousedown', () => normalizeLinks());
+        });
+    }
+
+    function normalizeLinks() {
+        document.querySelector('body[contenteditable]').querySelectorAll('a').forEach(link => {
+            if(link.target || link.target != '_target') {
+                link.target = '_target';
+            }
+            if(link.style) {
+                link.style = 'color: #3498db; text-decoration: underline;'
+            }
+        });
     }
 })();
