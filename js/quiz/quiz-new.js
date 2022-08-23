f34d851cc4d7f0336386bcd70e8ff3bb3564c42a
Venilton FalvoJr <falvojr@gmail.com>
2022-08-23 15:38:27 -0300

    Evolução no Quiz

--- js/quiz/quiz-new.js
+++ js/quiz/quiz-new.js
@@ -5,5 +5,7 @@
     const contentLastId = window.localStorage.getItem(storageKeys.content.lastOpenedId);
     if (contentLastId) {
         document.getElementById("id_content").value = contentLastId;
+        const optionSelectedText = document.querySelector(`#id_content option[value="${contentLastId}"]`).innerText;
+        document.getElementById('id_name').value = optionSelectedText;
     }
 })();
